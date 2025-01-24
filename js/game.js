1f2adc5f91df5c9f7ba15623b47c4e6f24d0c77c
PyroNave <joshuawa2002@gmail.com>
2025-01-24 15:24:20 +0200

    scds

--- js/game.js
+++ js/game.js
@@ -16,29 +16,24 @@ let rng = new Math.seedrandom(seed);
 var opponentCharacter;
 var allPics = [];
 
-fetch('./img/')
-  .then(response => response.text())
-  .then(data => {
-    const parser = new DOMParser();
-    const htmlDoc = parser.parseFromString(data, 'text/html');
-    const links = htmlDoc.querySelectorAll('a');
-
-    links.forEach(link => {
-      const val = link.getAttribute('href');
+$.ajax({
+  url: './img/',
+  success: function (data) {
+    $(data).find('a').attr('href', function (i, val) {
       if (val.match(/\.(jpe?g|png|gif)$/)) {
         allPics.push(val);
       }
     });
-
     if (allPics.length > 0) {
       setupGame();
     } else {
       console.error("No images found in img/ directory");
     }
-  })
-  .catch(error => {
-    console.error("Error loading images: ", error);
-  });
+  },
+  error: function (jqXHR, textStatus, errorThrown) {
+    console.error("Error loading images: " + textStatus + " " + errorThrown);
+  }
+});
 
 var selectedPics = [];
 
